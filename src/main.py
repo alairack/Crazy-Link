3ad1bcc29ffc6e0da37a65cd180c12617c760b8d
alairack <1477115009@qq.com>
2021-12-14 11:44:00 +0800

    修复了第一次打开窗口，两侧黑边过宽的问题

--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@ from check_openal import is_openal_exist, run_install_window
 from settings import setting
 import pyglet
 
+
 pyglet.media.get_audio_driver()           # 此代码理论上仅打印声音驱动名称，但可以使初次播放声音速度明显提升
 
 
 def run_game():
-    director.init(caption="Crazy Link", style=pyglet.window.Window.WINDOW_STYLE_DEFAULT, resizable=True)
+    director.init(caption="Crazy Link", style=pyglet.window.Window.WINDOW_STYLE_DEFAULT,
+                  width=setting.level_info[setting.level]["column"] * (setting.square_size + 2) + 30,
+                  height=setting.level_info[setting.level]["row"] * (setting.square_size + 2) + 65, resizable=True, vsync=True)
     director.window.set_icon(setting.logo)
     menu_scene = create_menu()
     director.run(menu_scene)
